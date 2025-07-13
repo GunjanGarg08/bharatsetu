9d7f9ad1fe5c6974f2810cc47499564a2b906f6d
Gunjan Garg <payalgarg18082828@gmail.com>
2025-07-13 16:32:09 +0530

    errors resolved

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,10 @@ import askRoute from "./routes/ask.js";
 
 dotenv.config();
 
+const allowedOrigins = [
+  'https://bharatsetu-git-main-gunjan-gargs-projects.vercel.app',
+];
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,9 +19,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
 // Routes
 app.use("/api/ask", askRoute);
